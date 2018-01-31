ac3a0254c75554a799db3b3e9637f167499b7131
Elizabeth Guerrero <eguerrer@vm133.sysnet.ucsd.edu>
2018-01-31 15:43:37 -0800

    after printing to file

--- Pictures/testers.py
+++ Pictures/testers.py
@@ -7,8 +7,8 @@ import os
 
 
 path = "/usr/local/home/eguerrer/PodLine/Pictures/"
-
-
+firstString = "";
+Ofile = open("TITLES.txt", "w")
 list = os.listdir(path)
 for im in list:
 	if im.endswith('.png'):
@@ -16,9 +16,26 @@ for im in list:
 		text = pytesseract.image_to_string(image, lang = 'eng')
 		title = text.partition("\n\n")[0]
 		filteredText = "".join(i for i in title if ord(i) <128)
-		inputPath = os.path.join(path, im)	
-		print("Next image: " +  im)	
-		print( "Next slide: " + filteredText)
-		print("\n")
+		# if firstString is empty 
+		# set firstString to filtered text
+		# and write to text
+		# else if first string is not empty
+		# compare the first String to filtered String
+		# if it is the same then don't write to text
+		# if it is not the same then do write it to the file
+		# assign first String to filtered text
+		firstString = Ofile.readline()
+		for st in Ofile
+			if firstString != filteredText
+				inputPath = os.path.join(path, im)	
+				Ofile.write("Next image:" + im)
+				Ofile.write("\n")
+				Ofile.write( "Next slide: " + filteredText)
+				Ofile.write("\n")
+				st = Ofile.readline()
+Ofile.close()
+
+
+		
 
 
